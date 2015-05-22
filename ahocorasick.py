4a9642e45a896bfe058f86da3409d72d5e8b4d27
David Moreno García <david.mogar@gmail.com>
2015-05-23 00:45:03 +0200

    Complete compute failures function
    I think that this is a valid solution but... who knows.

--- ahocorasick.py
+++ ahocorasick.py
@@ -8,18 +8,34 @@ trie['hers'] = 'hers'
 trie['his'] = 'his'
 
 def compute_failures():
-    node = trie._root
-    node.failure = node
+    root = trie._root
+    root.failure = root
 
     queue = deque()
 
-    for child in node.children.values():
-        child.failure = node
+    for child in root.children.values():
+        child.failure = root
         queue.append(child)
 
     while len(queue):
         node = queue.popleft()
-        for child in node.children.values():
+        for child_character, child in node.children.items():
             queue.append(child)
+            state = node.failure
+            found = False
+            state_failure = None
+            while found is False:
+                for state_character, state_child in state.children.items():
+                    state_failure = state_child
+                    if state_character == child_character:
+                        found = True
+                        break
+                if found is False:
+                    if state == root:
+                        found = True
+                        state_failure = root
+                    else:
+                        state = state.failure
+            child.failure = state_failure
 
 compute_failures()
